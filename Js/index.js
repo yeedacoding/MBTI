b98d25f166ba4fb0c204f6d9ffd0d528c7c16af9
yeedacoding <taeheun0325@naver.com>
2021-06-05 23:39:04 +0900

    Deleted unnecessary codes

--- Js/index.js
+++ Js/index.js
@@ -7,19 +7,3 @@ startBtn.addEventListener("click",function(event){
         event.preventDefault();
     }
 });
-
-
-// 시작하기 버튼을 눌렀을 때 실행되는 것
-// function begin(){
-//     main.style.animation= "fadeOut 1s";
-//     testQuestions.style.animation = "fadeIn 1s";
-//     //main.style.display = "none";
-//     //testQuestions.style.display = "block";
-// }
-const main = document.querySelector(".mainScreen");
-const testQuestions = document.querySelector(".testQuestions");
-
-function beginTest(){
-    main.style.display = "none";
-    testQuestions.style.display = "block";
-}
